4086ea113f15711e2193dcba6d1cae9c6c7d1b3b
Anorexiabarn <patrickjames.dougherty@elev.ga.ntig.se>
2019-09-03 10:10:52 +0200

    Första 80 minuterna på tisdag, 03/09

--- lek1.py
+++ lek1.py
@@ -1,1 +1,10 @@
 print("hej blyat jag är putnick")
+
+firstName = "Patrick"
+lastName = "Dougherty"
+dateOfBirth = 20010510
+
+age = 18
+teacher = False
+print (firstName, lastName + str(age) + str(teacher))
+print (dateOfBirth +" ")
